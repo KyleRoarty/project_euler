22bb2e6f3532abe0dabec7aaa0052773132c4378
Kyle Roarty <kyleroarty1716@gmail.com>
2017-11-30 12:14:29 -0600

    Subtract instead of add
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- p31.py
+++ p31.py
@@ -2,17 +2,17 @@
 
 import time
 
-goal=200
+goal=0
 valueLUT = [200, 100, 50, 20, 10, 5, 2, 1]
 
 def genTree(num, LUT):
     combs = 0
     for i in range(0, len(LUT)):
-        if num + LUT[i] > goal:
+        if num - LUT[i] < goal:
             continue
-        elif num + LUT[i] < goal:
-            combs += genTree(num+LUT[i], LUT[i:])
-        elif num + LUT[i] == goal:
+        elif num - LUT[i] > goal:
+            combs += genTree(num - LUT[i], LUT[i:])
+        elif num - LUT[i] == goal:
             combs += 1
 
     return combs
@@ -20,7 +20,7 @@ def genTree(num, LUT):
 
 start = time.clock()
 
-numCombs = genTree(0, valueLUT)
+numCombs = genTree(200, valueLUT)
 print(numCombs)
 
 end = time.clock()
