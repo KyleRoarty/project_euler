5c08889e1c279748564d5797257c000fc6782b4a
Kyle Roarty <kyleroarty1716@gmail.com>
2017-11-30 10:35:56 -0600

    Remove tree data structure, just use numbers.
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- p31.py
+++ p31.py
@@ -2,55 +2,26 @@
 
 import time
 
-WAYS = []
 goal=200
 valueLUT = [200, 100, 50, 20, 10, 5, 2, 1]
-dict_template = {1:0, 2:0, 5:0, 10:0, 20:0, 50:0, 100:0, 200:0}
-COUNT = 0
 
-
-class Tree(object):
-    def __init__(self, value=0, total=0, parent=None, children=[]):
-        self.value=value
-        self.total=total
-        self.parent=parent
-        self.children=children
-        self.count=0
-
-    def add_child(self,node):
-        assert isinstance(node, Tree)
-        self.children.append(node)
-
-def getCombination(node):
-    new_dict = dict_template.copy()
-    search_node = node
-
-    while search_node.parent != None:
-        new_dict[search_node.value] += 1
-        search_node = search_node.parent
-
-    search_node.count += 1
-
-def genTree(node, LUT):
+def genTree(num, LUT):
+    combs = 0
     for i in range(0, len(LUT)):
-        if node.total + LUT[i] > goal:
+        if num + LUT[i] > goal:
             continue
+        elif num + LUT[i] < goal:
+            combs += genTree(num+LUT[i], LUT[i:])
+        elif num + LUT[i] == goal:
+            combs += 1
 
-        new_node = Tree(LUT[i], node.total+LUT[i], node)
-        node.add_child(new_node)
-
-        if node.total + LUT[i] < goal:
-            genTree(new_node, LUT[i:])
-        elif node.total + LUT[i] == goal:
-            getCombination(new_node)
+    return combs
 
 
 start = time.clock()
 
-root = Tree()
+numCombs = genTree(0, valueLUT)
+print(numCombs)
 
-genTree(root, valueLUT)
-
-print(root.count)
 end = time.clock()
 print(end-start)
