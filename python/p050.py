5df0250d66794716809711838dbb6c6693737dec
Kyle Roarty <kyleroarty1716@gmail.com>
2019-02-24 16:59:37 -0600

    Working p050. Much better e_sieve
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

---
+++ python/p050.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+'''
+The prime 41, can be written as the sum of six consecutive primes:
+41 = 2 + 3 + 5 + 7 + 11 + 13
+
+This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+
+The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+
+Which prime, below one-million, can be written as the sum of the most consecutive primes?
+
+'''
+
+def better_e_sieve(max_n):
+    prime = [True]*max_n
+    p = 2
+    ret_l = []
+
+    while p*p <= max_n:
+        if prime[p]:
+            for i in range(p*2, max_n, p):
+                prime[i] = False
+
+        p += 1
+
+    for p in range(2, max_n):
+        if prime[p]:
+            ret_l.append(p)
+
+    return ret_l
+
+def e_sieve(max_n):
+    to_sieve = list(range(2, max_n))
+    primes = []
+
+    while to_sieve:
+        primes.append(to_sieve[0])
+        to_sieve = list(filter(lambda x: x % to_sieve[0], to_sieve))
+
+    return primes
+
+MAX_N = 1000000
+
+primes = better_e_sieve(MAX_N)
+
+for i in range(len(primes)-1, 1, -1):
+    j = 0
+    sum_cons = sum(primes[0:i-1])
+    while j + i < len(primes):
+        sum_cons += primes[i+j-1]
+
+        if sum_cons > primes[len(primes)-1]:
+            break
+
+        if sum_cons in primes:
+            print('Winner: {}, {}, {}'.format(sum_cons, i, j))
+            quit()
+
+        sum_cons -= primes[j]
+        j += 1
