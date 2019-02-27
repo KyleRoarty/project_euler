6feb22961343792241bef801bd34e3679266e9dc
Kyle Roarty <kyleroarty1716@gmail.com>
2019-02-26 20:32:18 -0600

    p010: re-write, use python3, use sieve instead of slow prime getting
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- python/p010.py
+++ python/p010.py
@@ -1,18 +1,20 @@
-import math
-primes = [2,3,5,7,11,13]
-total = 2+3+5+7+11+13
-potPr = 15
-while potPr < 2000000:
-    index = 0
-    test = math.floor(math.sqrt(potPr))
-    for index in range(0,len(primes)):
-        if potPr % primes[index] == 0:
-            break
-        if primes[index] > test:
-            primes.append(potPr)
-            total += potPr
-            break
-    potPr += 2
-    if(potPr % 10000 == 1):
-        print potPr
-print total
+#!/usr/bin/env python3
+
+from euler import *
+from time import process_time
+from functools import reduce
+
+'''
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+'''
+
+start = process_time()
+
+MAX_N = 2000000
+primes = sieve1(MAX_N)
+
+total = reduce(lambda x,y: x+y, primes)
+
+print('Winner: {}\nTime: {}'.format(total, process_time()-start))
