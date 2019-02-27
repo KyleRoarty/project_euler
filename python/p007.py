d29e832184015e92a9fe042ebd357f72aef5e9b0
Kyle Roarty <kyleroarty1716@gmail.com>
2019-02-26 20:26:00 -0600

    p007: Re-write, use python3, about 2x as fast but still slow
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- python/p007.py
+++ python/p007.py
@@ -1,13 +1,25 @@
-primes = [2,3,5,7,11,13]
-potPr = 15
-count = 6
+#!/usr/bin/env python3
+
+from euler import *
+from time import process_time
+
+'''
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+'''
+
+
+start = process_time()
+
+primes = [2]
+count = 1
+pot_p = 3
+
 while count < 10001:
-    index = 0
-    for index in range(0,len(primes)):
-        if potPr % primes[index] == 0:
-            break
-        if index == len(primes)-1:
-            primes.append(potPr)
-            count += 1
-    potPr += 2
-print primes[10000]
+    if is_prime(pot_p, primes):
+        primes.append(pot_p)
+        count += 1
+    pot_p += 2
+
+print('Winner: {}\nTime: {}'.format(primes[10000], process_time()-start))
